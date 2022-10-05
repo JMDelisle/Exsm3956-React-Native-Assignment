f18a02e94f08c59277766a44e6c50998f9a7ab75
Jean-Marc Delisle <delisle.jeanmarc@hotmail.com>
2022-10-05 15:28:18 -0600

    Fixed my issue I had with navigation and also had worked on countdown, need to adjust StyleSheet on main page;

--- App.js
+++ App.js
@@ -1,86 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import { StyleSheet, View, Button, Text } from 'react-native'
-import DatePicker from 'react-native-neat-date-picker'
-import moment from 'moment/moment'
+import { StyleSheet, View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import HappyBirthday from './HappyBirthday'
+import HappyBirthdayCountdown from './HappyBirthdayCountdown'
 
-const App = () => {
-  const [showDatePickerSingle, setShowDatePickerSingle] = useState(false)
-
-  const [date, setDate] = useState('');
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const openDatePickerSingle = () => setShowDatePickerSingle(true)
-
-  // const new_today = Date.now();
-
-  // var userDate = new Date().getDate(); //To get the Current Date
-  var diff_date = moment().format('YYYY-MM-DD');
-  // var userDate = new Date().getDate().padStart(2, '0');
-  // var month = new Date().getMonth() + 1; //To get the Current Month
-  // var year = new Date().getFullYear(); //To get the Current Year
-  // const todaydate = `${year}-${month}-${userDate}`;
-  // const todaydate = `${currentDate.getFullYear()}-${currentDate.getMonth()+1}-${currentDate.getDate()}`;
-
-  
-
-  const onCancelSingle = () => {
-    // You should close the modal in here
-    setShowDatePickerSingle(false)
-  }
-
-  const onConfirmSingle = (output) => {
-    // You should close the modal in here
-    setShowDatePickerSingle(false)
-
-    // The parameter 'output' is an object containing date and dateString (for single mode).
-    // For range mode, the output contains startDate, startDateString, endDate, and EndDateString
-    console.log(output)
-    setDate(output.dateString)
-  }
-
-// useEffect(() => {
-//   if(date === date.today){
-//     //goto page
-//     console.log(date)
-//   } else {
-//     // go to other page 
-//     console.log(date)
-//   }, [date]);
- 
- 
-useEffect(() => {
-  if(date === diff_date){
-  console.log("they are the same date!")
-  } else {
-  console.log(diff_date)
-}}, [date]);
+// import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Click below to determine your date of birth.</Text>
-      {/* Single Date */}
-      
-      <Button title={'Click Me'} onPress={openDatePickerSingle} />
-      <Button title="Today?" disabled onPress={() => Alert.alert('Must be todays date!')} />
-      <DatePicker
-        isVisible={showDatePickerSingle}
-        mode={'single'}
-        onCancel={onCancelSingle}
-        onConfirm={onConfirmSingle}
-
-
-      />
-      <Text>{date}</Text>
-
-      
-      <Text>{startDate && `${startDate} ~ ${endDate}`}</Text>
-    </View>
-  )
+    <NavigationContainer>
+        <View style={styles.container}>
+          {/* Anything outside of the Stack.Navigator will be persistent across screens (useful for menus). */}
+          <Text>Exsm3956- React Native </Text>
+          <Stack.Navigator>
+            <Stack.Screen name="HappyBirthday" component={HappyBirthday} />
+            <Stack.Screen name="HappyBirthdayCountDown" component={HappyBirthdayCountdown} />
+          </Stack.Navigator>
+          <StatusBar style="auto" />
+        </View>
+    </NavigationContainer>
+  );
 }
 
-export default App
+
 
 const styles = StyleSheet.create({
   container: {
--- HappyBirthday.js
+++ HappyBirthday.js
@@ -1,24 +1,89 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, Button, Text } from 'react-native'
+import DatePicker from 'react-native-neat-date-picker'
+import moment from 'moment/moment'
+import { Navigation } from 'react-calendar'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const App = ({ navigation }) => {
+  const [showDatePickerSingle, setShowDatePickerSingle] = useState(false)
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [date, setDate] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const openDatePickerSingle = () => setShowDatePickerSingle(true);
+  // const new_today = Date.now();
+
+  var diff_date = moment().format('YYYY-MM-DD');
+
+  const onCancelSingle = () => {
+    // You should close the modal in here
+    setShowDatePickerSingle(false)
+  }
+
+  const onConfirmSingle = (output) => {
+    // You should close the modal in here
+    setShowDatePickerSingle(false)
+
+    // The parameter 'output' is an object containing date and dateString (for single mode).
+    // For range mode, the output contains startDate, startDateString, endDate, and EndDateString
+    console.log(output)
+    setDate(output.dateString)
+  }
+
+  useEffect(() => {
+    setIsDisabled(false);
+  }, [date]);
 
 
-export default function HappyBirthday({ navigation, route }) {
+
+
+
   return (
-    <View style={styles.container}>
-      {/* Since we gave a param called test to navigate, we can pull it out here. Just be careful if you have multiple methods of navigation that you don't wind up with a null reference. */}
-      <Text>Happy Birthday!, {route.params.test}</Text>
-      <Button title="Go back to date picker" onPress={() => {
-        // Navigate can also just accept the name of the screen, without params.
-        navigation.navigate('App');
-      }} />
-    </View>
-  );
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.row}>Click below to determine your date of birth.</Text>
+        {/* Single Date */}
+
+        <Button title={'Click Me'} onPress={openDatePickerSingle} style={styles.button} />
+        <Button value={date} title='Today?' disabled={isDisabled} onPress={() => navigation.navigate('HappyBirthdayCountDown',{paramKey: date})} style={styles.button} />
+        {/* <Button title="Go to Birthday-Count-Down" onPress={() => { navigation.navigate('HappyBirthdayCountDown'); 
+      }}/> */}
+
+
+        {/* <Button title="Today?" disabled onPress={() => Alert.alert('Must be todays date!')} /> */}
+        <DatePicker
+          isVisible={showDatePickerSingle}
+          mode={'single'}
+          onCancel={onCancelSingle}
+          onConfirm={onConfirmSingle}
+
+
+        />
+        <Text>{date}</Text>
+
+
+        <Text>{startDate && `${startDate} ~ ${endDate}`}</Text>
+      </View>
+    </SafeAreaView>
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    width: 350,
+    padding: 75,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green'
   },
-});
+  row: {
+    padding: 4,
+    borderBottomColor: "red",
+    borderBottomWidth: StyleSheet.hairlineWidth
+  }
+
+})
